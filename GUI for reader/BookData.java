b94c5d07fc948934bb0e68e80fc62587227b2e36
belfaunder <belfaunder1@gmail.com>
2014-04-18 16:56:18 +0400

    Revert "Добавлено чтение из txt, которое сделала Альбина, и дальше работаю над"
    This reverts commit 22ebaaa0f7be949911eba8c75390de6b70fb691b.

--- GUI for reader/BookData.java
+++ GUI for reader/BookData.java
@@ -4,130 +4,53 @@ import javax.swing.*;
 
 public class BookData extends JFrame{
 	
-	int PROPERTIES = 7;
-	
-    public Container createContentPane() {
+    public Container createContentPane() {	//WITHOUT LAYOUT YET
     	
         JPanel contentPane = new JPanel();
         
-        GridLayout dataLayout = new GridLayout(PROPERTIES,2);
-        contentPane.setLayout(dataLayout);
-        
         JLabel authorLabel = new JLabel("Автор");
         JTextField author = new JTextField("", 25);
-  //      contentPane.add(authorLabel);
-  //      contentPane.add(author);
+        contentPane.add(authorLabel);
+        contentPane.add(author);
         
         JLabel titleLabel = new JLabel("Название");
-        JTextField title = new JTextField("", 25);
-  //      contentPane.add(titleLabel);
-  //      contentPane.add(title);
+        JTextField title = new JTextField("", 22);
+        contentPane.add(titleLabel);
+        contentPane.add(title);
         
         JLabel genreLabel = new JLabel("Жанр");
-        JTextField genre = new JTextField("", 25);
-  //      contentPane.add(genreLabel);
-  //      contentPane.add(genre);
+        JTextField genre = new JTextField("", 23);
+        contentPane.add(genreLabel);
+        contentPane.add(genre);
 
         JLabel seriesLabel = new JLabel("Серия");
-        JTextField series = new JTextField("", 25);
-  //      contentPane.add(seriesLabel);
-  //      contentPane.add(series);
+        JTextField series = new JTextField("", 20);
+        contentPane.add(seriesLabel);
+        contentPane.add(series);
         
         JLabel yearLabel = new JLabel("Год выпуска");
-        JTextField year = new JTextField("", 25);
-  //      contentPane.add(yearLabel);
-  //      contentPane.add(year);
+        JTextField year = new JTextField("", 15);
+        contentPane.add(yearLabel);
+        contentPane.add(year);
         
         JLabel modeLabel = new JLabel("Режим чтения");
-        JTextField mode = new JTextField("", 25);
-  //      contentPane.add(modeLabel);
-  //      contentPane.add(mode);
+        JTextField mode = new JTextField("", 19);
+        contentPane.add(modeLabel);
+        contentPane.add(mode);
         
         JLabel tagsLabel = new JLabel("Тэги");
-        JTextField tags = new JTextField("", 25);
-  //      contentPane.add(tagsLabel);
-  //      contentPane.add(tags);
-
+        JTextField tags = new JTextField("", 20);
+        contentPane.add(tagsLabel);
+        contentPane.add(tags);
         
-        GroupLayout layout = new GroupLayout(contentPane);
-        contentPane.setLayout(layout);
-        layout.setAutoCreateGaps(true);
-        layout.setAutoCreateContainerGaps(true);
-        
-        layout.setHorizontalGroup(
-        		
-        		layout.createSequentialGroup()
-
-        				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-        					.addComponent(authorLabel, GroupLayout.PREFERRED_SIZE,
-        						GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-        					.addComponent(titleLabel)
-        					.addComponent(genreLabel)
-        					.addComponent(seriesLabel)
-        					.addComponent(yearLabel)
-        					.addComponent(modeLabel)
-        					.addComponent(tagsLabel))
-        				
-        				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-        					.addComponent(author, GroupLayout.PREFERRED_SIZE,
-                				GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                			.addComponent(title)
-                			.addComponent(genre)
-                			.addComponent(series)
-                			.addComponent(year)
-                			.addComponent(mode)
-               				.addComponent(tags)));
-
- //        layout.linkSize(SwingConstants.HORIZONTAL,
- //      		  new java.awt.Component[]{authorLabel, author});
-          
-          layout.setVerticalGroup(
-
-        	   layout.createSequentialGroup()
-        		  
-        		  .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-        				.addComponent(authorLabel, GroupLayout.PREFERRED_SIZE,
-        								  GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-        				.addComponent(author))
-        								 
-        		  .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-          				.addComponent(titleLabel, GroupLayout.PREFERRED_SIZE,
-          								  GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)			
-        				.addComponent(title))
-        
-          		  .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-        				.addComponent(genreLabel, GroupLayout.PREFERRED_SIZE,
-        								  GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-          				.addComponent(genre))
-        
-          	   	 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-        				.addComponent(seriesLabel, GroupLayout.PREFERRED_SIZE,
-        								  GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)			
-        				.addComponent(series))
-        
-            	 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                		.addComponent(yearLabel, GroupLayout.PREFERRED_SIZE,
-                						  GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-              			.addComponent(year))
-              
-                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-              			.addComponent(modeLabel, GroupLayout.PREFERRED_SIZE,
-              							  GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                		.addComponent(mode))
-              
-              	 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-              			.addComponent(tagsLabel, GroupLayout.PREFERRED_SIZE,
-              						  GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-              			.addComponent(tagsLabel)));
-        
-           pack();
-           
         return contentPane;
+    	
     }
 	
 	public static void createAndShowGUI() {
 
         JFrame frame = new JFrame("Данные о книге");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         BookData data = new BookData();
         frame.setContentPane(data.createContentPane());
         frame.setSize(350, 220);
@@ -135,7 +58,9 @@ public class BookData extends JFrame{
 		frame.setLocationRelativeTo(null);
         frame.setAlwaysOnTop(true);
         frame.setVisible(true);
+
         
     }
+	
 
 }
